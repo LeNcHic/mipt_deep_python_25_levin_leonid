97bca90ecc7150119ce8d1cfc86cd0cb53dc6444
Leonid <89319228+LeNcHic@users.noreply.github.com>
2025-09-27 12:28:45 +0300

    task01
    * Add config files
    
    * Hw1 with coverage pylint flake8
    
    ---------
    
    Co-authored-by: Leonid <levin.li@phystech.edu>

---
+++ hw_task01/tests/test_predict_message_mood.py
@@ -0,0 +1,23 @@
+"""Тесты для predict_message_mood"""
+
+
+from hw_task01.predict_message_mood import predict_message_mood
+
+
+def test_returns_best():
+    """Тест на отл"""
+    assert predict_message_mood("Чапаев и пустота") == "отл"
+
+
+def test_returns_norm():
+    """Тест на норм"""
+    assert predict_message_mood(
+        "Чапаев и пустота",
+        bad_thresholds=0.8,
+        good_thresholds=0.99,
+    ) == "норм"
+
+
+def test_returns_bad():
+    """Тест на неуд"""
+    assert predict_message_mood("Вулкан") == "неуд"
